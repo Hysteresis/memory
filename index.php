9e8bccb2de3a11581ae52ca0394d8181c4da5cab
Hysteresis <piballz@gmail.fr>
2022-04-26 08:57:10 +0200

    init

--- index.php
+++ index.php
@@ -4,19 +4,26 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         
-        <script defer src="app/grid.js"></script>
+
+        <script defer src="app/cardArray.js"></script>
+        <script defer src="app/initGrid.js"></script>
+        <script defer src="app/clickCard.js"></script>
+        
+        <!-- TODO ajouter les fichiers JS manquants -->
+        <!-- <script defer src="app/"></script> -->
         <link rel="stylesheet" href="style.css">    
         <title>memory</title>
 </head>
 <body>
         <h1>Memory</h1>
         <p id="is_js"></p>
+        
 
         <div id="grid-container">
-                
+            
 
         </div>
+        
 
 
 
