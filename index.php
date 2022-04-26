17adeb1060e95717142f25832ef789a5f61b5078
Hysteresis <piballz@gmail.fr>
2022-04-26 16:25:09 +0200

    ok

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <script defer src="app/cardArray.js"></script>
-        <script defer src="app/initGrid.js"></script>
-        <script defer src="app/clickCard.js"></script>
+        <script defer src="assets/app/app.js"></script>
+
         
         <!-- TODO ajouter les fichiers JS manquants -->
         <!-- <script defer src="app/"></script> -->
@@ -16,6 +15,16 @@
 </head>
 <body>
         <h1>Memory</h1>
+        <p id="is_victory"></p>
+        <p id="score"></p>
+       
+        
+        <label for="progressBar">Temps </label>
+        <progress id="progressBar" max="100" value="0"> 70% </progress>
+        
+        
+       
+        
         <p id="is_js"></p>
         
 
